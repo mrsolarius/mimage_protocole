aa9ce854fb0c0db98d1903994147b724d1e7f80a
l3 miage-bray <77206709+l3miage-braya@users.noreply.github.com>
2021-11-30 12:17:21 +0100

    feature issus #3: fin du trameData.h

--- header/data_trame.h
+++ header/data_trame.h
@@ -78,40 +78,7 @@ char* encodeDataHead(struct dataTrame);
 struct dataTrame decodeDataHead(char * data);
 
 
-/**
-  * Nom :
- *      sendDataTrame - envoie la dataTrame passé en paramètre.Il retourne 0 si succès sinon -1 erreur.
- * Description :
- *      Récupère les éléments de la structure et retourne rien.
- *      Si dataTrame est vide alors il envoie un message d'erreur 0 de type dataTrameVide.
- *      Si dataTrame a un cmd inexistant, alors il il envoie un message d'erreur 1 de type cmdVide.
- *      Si dataTrame a un cmd ne corresponds pas à liste de commande,alors il il envoie un message d'erreur 2 de type cmdErreur.
- *      Si dataTrame a un status inexistant, alors il il envoie un message d'erreur 3 de type statusVide.
- *      Si dataTrame a un status ne corresponds pas à liste de commande,alors il il envoie un message d'erreur 4 de type statusErreur.
- *      Si dataTrame a un sizeData inexistant, alors il il envoie un message d'erreur 5 de type sizeDataVide.
- * Paramètre :
- *      @param struct dataTrame corresponds à la structure générer par le serveur (à partir de l'analyse de fichier créer par celui -ci) 
- **/
 
-
-int sendDataTrame( struct dataTrame);
-
-/**
-  * Nom :
- *      receiveData - recoit le dataHead et un file descripteur. Il retourne 0 si succès sinon -1 erreur.
- * Description :
- *      recoit le dataHead et le descripteur 
- *      
- *      Si dataTrame est vide alors il envoie un message d'erreur 0 de type dataTrameVide.
- *      Si dataTrame a un cmd inexistant, alors il il envoie un message d'erreur 0 de type cmdVide.
- *      Si dataTrame a un cmd ne corresponds pas à liste de commande,alors il il envoie un message d'erreur 0 de type cmdErreur.
- *      Si dataTrame a un status inexistant, alors il il envoie un message d'erreur 0 de type statusVide.
- *      Si dataTrame a un status ne corresponds pas à liste de commande,alors il il envoie un message d'erreur 0 de type statusErreur.
- *      Si dataTrame a un sizeData inexistant, alors il il envoie un message d'erreur 0 de type sizeDataVide.
- * Paramètre :
- *      @param char* head, int fileFD correspond à l'entete de dataTrame et le descripteur
- **/
-int receiveData(char*  head, int fileFD);
 
 
 

0bc96742f72e349d42af8e0d7885557afe66b427
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-11-30 23:20:43 +0100

    Test : Merge des tests de SPP

--- headers/all-test.h
+++ headers/all-test.h
@@ -1,10 +1,11 @@
 #ifndef all_test_h
 #define all_test_h
-#include <stdbool.h>
 // fonction de tests du core
 void testCore();
 
 //fonction de test du type
 void testType();
-bool checkTypeMp4_itShouldPass_1();
+
+//fonction de test de Send Picture Protocole
+void testSPP();
 #endif // all_test_h

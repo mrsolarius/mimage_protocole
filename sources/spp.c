6ba2c501f05d7ab1c379498fdbc0bb27667ae550
l3 miage-bray <77206709+l3miage-braya@users.noreply.github.com>
2021-11-30 18:18:00 +0100

    feature issus #4 avancement des tests unitaires (to do modif)

---
+++ sources/spp.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <../header/spp.h>
+
+char* encodeInfosTrame(struct infosTrame){}
+
+struct infosTrame decodeInfosTrame(char* infos, unsigned int size){}

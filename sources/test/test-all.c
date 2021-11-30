5d212478e63e14dafc48e84057d17df738ff1438
Vaaanir <46886434+Vaaanir@users.noreply.github.com>
2021-11-30 11:48:44 +0100

    Update test-all.c
    Fix : comment

--- sources/test/test-all.c
+++ sources/test/test-all.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     //test du core de test
     testCore();
-    //afichage du résumé des tests
+    //affichage du résumé des tests
     resume();
     return 0;
 }

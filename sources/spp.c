17d12f7f96b68cef677f1bac9068b3ba8640eea3
JuJu-2404 <92156764+JuJu-2404@users.noreply.github.com>
2021-12-01 09:44:41 +0100

    Feature:#6 & #7 Implémentation de InforTrame et de DataTrame.

--- sources/spp.c
+++ sources/spp.c
@@ -23,8 +23,49 @@ bool checkInfoTrameError(PInfoTrame infosTrame){
         SPP_Erno=EMPTY_STATUS;
         test=true;
     }
+    if ((infosTrame->status!=SUCCESS)||
+        !((infosTrame->status>=0x41)&&(infosTrame->status<=0x45))||
+        !((infosTrame->status>=0x50)&&(infosTrame->status<=0x52))){
+        SPP_Erno=STATUS_ERROR;
+        test=true;
+        }
     if ((infosTrame->sizeInfos!=strlen(infosTrame->infos))){
         SPP_Erno=WRONG_SIZE;
+        test=true;
+    }
+    return test;
+}
+
+bool checkDataTrameError(PDataTrame dataTrame){
+    bool test=false;
+    if (dataTrame->cmd==0){
+        SPP_Erno=EMPTY_CMD;
+        test=true;
+    }
+    if ((dataTrame->cmd!=DOWNLOAD_FILE_NAME)||(dataTrame->cmd!=UPLOAD_FILE_DATA)){
+        SPP_Erno=CMD_ERROR;
+        test=true;
+    }
+    if (dataTrame->status==0){
+        SPP_Erno=EMPTY_STATUS;
+        test=true;
+    }
+    if ((dataTrame->status!=SUCCESS)||
+        !((dataTrame->status>=0x41)&&(dataTrame->status<=0x45))||
+        !((dataTrame->status>=0x50)&&(dataTrame->status<=0x52))){
+        SPP_Erno=STATUS_ERROR;
+        test=true;
+        }
+    if (dataTrame->dataFd==0){
+        SPP_Erno=EMPTY_FD;
+        test=true;
+    }
+    if (dataTrame->dataFd<0){
+        SPP_Erno=FD_ERROR;
+        test=true;
+    }
+    if ((dataTrame->status==SUCCESS)&&(dataTrame->sizeData==0)){
+        SPP_Erno=EMPTY_SIZE_DATA;
         test=true;
     }
     return test;
@@ -60,3 +101,34 @@ PInfoTrame decodeInfosTrame(char* infos, unsigned int size){
     return TrameDecod;
 }
 
+char* encodeDataHead(PDataTrame dataTrame){
+        // on check tous les problèmes potentiel pouvant subevenir
+    if (checkDataTrameError(dataTrame)==true){
+        return (char *) (0xFF);
+    }
+     char * trame= malloc(sizeof(char)*6); //soit la taille de 2 char et d'un long
+    trame[0]=dataTrame->cmd;
+    trame[1]=dataTrame->status;
+    trame[2]=(dataTrame->sizeData>>24)& 0xFF; // On décalle de 24 bytes puis on prend un masque de 4 bytes pour le faire correspondre à un char.
+    trame[3]=(dataTrame->sizeData>>16)& 0xFF; // On décalle de 16 bytes puis on prend un masque de 4 bytes pour le faire correspondre à un char.
+    trame[4]=(dataTrame->sizeData>>8)& 0xFF; // On décalle de 8 bytes puis on prend un masque de 4 bytes pour le faire correspondre à un char.
+    trame[5]=(dataTrame->sizeData)& 0xFF;
+    return trame;
+}
+
+PDataTrame decodeDataHead(char * data, int dataFd){
+    PDataTrame trameDecod = (PDataTrame) malloc(sizeof(DataTrame));
+    trameDecod->cmd=data[0];
+    trameDecod->status=data[1];
+    trameDecod->sizeData=0;
+    trameDecod->sizeData|=(data[2]& 0xFF)<<24;
+    trameDecod->sizeData|=(data[2]& 0xFF)<<16;
+    trameDecod->sizeData|=(data[2]& 0xFF)<<8;
+    trameDecod->sizeData|=(data[2]& 0xFF);
+    trameDecod->dataFd=dataFd;
+    // on check tous les problèmes potentiel pouvant subevenir
+    if (checkDataTrameError(trameDecod)==true){
+        return (char *) (0xFF);
+    }
+    return trameDecod;
+}

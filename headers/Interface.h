9c986c950124bae46ff0290a7330b843e0401909
JuJu-2404 <92156764+JuJu-2404@users.noreply.github.com>
2021-12-03 13:36:05 +0100

    Feat #27: Manque l'envoie de fichier

--- headers/Interface.h
+++ headers/Interface.h
@@ -4,8 +4,16 @@
 #define CBG "\u255D" 
 #define MH "\u2550" 
 #define MV "\u2551" 
+//sert de main de remplacement, attention c'est temporaire
 void Affiche();
+//affiche le menu principal du client
 void menu();
+//affiche la liste des fichiers présents sur le serveur
 void liste();
-void envoie();
+//Permet de choisir le fichier à envoyer
+char * envoie();
+//permet de récupérer le nom du fichier qui doit être téléchargé (les fichiers sont identifiées par leurs noms)(il donne le numéro du nom dans les fait)
 int recuperation();
+//affiche des ligne de séparation
+void ligne();
+void curseur();
--- sources/Interface.c
+++ sources/Interface.c
@@ -4,28 +4,41 @@
 #include "Interface.h"
 
 void liste(char ** nom, int taille){
-    fprintf(stdout,"Voici la liste des fichiers présents sur le serveur :\n");
+    fprintf(stdout,"\tVoici la liste des fichiers présents sur le serveur :\n");
     for(int comp=0;comp<taille;comp++){
-        fprintf(stdout,"\t>%d - %s\n",comp,nom[comp]); 
+        fprintf(stdout,"\t\t>%d - %s\n",comp,nom[comp]); 
     }
 }
 
-void envoie(){
+char * envoie(){
+    char *chemin;
+    printf("\tMerci de renseigner le chemin du fichier que vous souhaitez envoyer :");
+    return chemin;
 }
 
-char ** recuperation(char ** nom, int taille){
-    char * saisie= malloc(sizeof(char));
-    fprintf(stdout,"\tQuel fichier voulez vous récupérer ? Merci d'entrer le numéro correspondant:\n");
-    fscanf(stdin,"%c",saisie);
-    saisie=atoi(saisie);
+void ligne(){
+    printf("============================================================================================================================\n");
+}
+
+void curseur(){
+    printf("\n\t>:");
+}
+
+int recuperation(char ** nom, int taille){
+    int saisie;
+    char lect[255];
+    fprintf(stdout,"\tQuel fichier voulez vous récupérer ? Merci d'entrer le numéro correspondant:");
+    curseur();
+    fscanf(stdin,"%s",lect);
+    saisie=atoi(lect);
     while((saisie>taille)||(saisie<0)){
-        fprintf(stdout,"Mauvais numéro de fichier. Merci d'entrer un numéro compris dans la liste:");
-        fscanf(stdin,"%c",saisie);
-        saisie=atoi(saisie);
+        fprintf(stdout,"\t\t\t\t/!\\ Mauvais numéro de fichier. Merci d'entrer un numéro compris dans la liste:\t /!\\");
+        curseur();
+        liste(nom,taille);
+        fscanf(stdin,"%s",lect);
+        saisie=atoi(lect);
     };
-    char ** retour= (char **) malloc(sizeof(char));
-    
-    return 0;
+    return saisie;
 }
 
 void menu(){
@@ -36,9 +49,10 @@ void menu(){
     printf(MV "\t\t\t1 - Consulter la liste de fichier\t\t\t\t\t\t\t\t " MV"\n");
     printf(MV "\t\t\t2 - Envoyer un fichier sur le serveur\t\t\t\t\t\t\t\t " MV"\n");
     printf(MV "\t\t\t3 - Récupérer un fichier depuis le serveur\t\t\t\t\t\t\t " MV"\n");
-    printf(MV "\t\t\t4 - Arrêter le client et quitter la connexion\t\t\t\t\t\t\t " MV"\n");
     printf(MV "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t "MV"\n");
-    printf(CBD MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH CBG"\n");
+    printf(MV "\t\t\t0 [Default] - Arrêter le client et quitter la connexion\t\t\t\t\t\t " MV"\n");
+    printf(MV "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t "MV"\n");
+    printf(CBD MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH MH CBG);
 }
 
 
@@ -46,37 +60,59 @@ void Affiche(){
     char * choix= malloc(sizeof(char));
 
     //exemples de noms
-    char str[] ="voiture";
+    char str[] ="voiture.png";
     char ** files = (char **) malloc(sizeof(char *) * 2);
     files[0] = (char *) malloc(sizeof(char) * (strlen(str)+1));
     strcpy(files[0],str);  
     
-    char str2[] ="camion";
+    char str2[] ="camion.ico";
     files[1] = (char *) malloc(sizeof(char) * (strlen(str)+1));
     strcpy(files[1],str2); 
     int taille=2;
     
-    
+    int index;
+    int quit2=1;
     int quit=0;
     do{
         menu();
+        curseur();
         fscanf(stdin,"%s",choix);
         switch  (atoi(choix)){
             case 1:
+                ligne();
                 liste(files,taille);
+                ligne();
                 break;
             case 2:
+                ligne();
                 envoie();
+                ligne();
                 break;
             case 3:
-                liste(files,taille);
-                recuperation(files,taille);
+                do{
+                    ligne(); 
+                    liste(files,taille);
+                    index=recuperation(files,taille); //on récupère l'indice du nom du fichier à récup
+                    //mettre le téléchargement du fichier ici @todo
+                    printf("\t%s\n\tVoulez-vous récupérer un autre fichier ?\n\t\tEntrer 0 pour revenir au menu principal.\n\t\tEntrer 1 pour récupérer un autre fichier.",files[index]);
+                    curseur();
+                    scanf("%s",choix);
+                    if ((atoi(choix)!=1&&atoi(choix)!=0)){
+                        printf("\t\t/!\\Merci de rentrer une valeur valide /!\\");
+                        curseur();
+                        scanf("%s",choix);
+                    }
+                    quit2=atoi(choix);
+                    ligne();
+                }
+                while(quit2!=0);
                 break;
-            case 4:
+            case 0:
                 quit=1;
                 break;
             default:
-                fprintf(stdout,"\n\t\t\t\t/!\\ Merci d'entrer une valeur présente dans la liste ! /!\\ \n");
+                fprintf(stdout,"\n\t\t\t\t/!\\ Merci d'entrer une valeur présente dans la liste ! /!\\");
+                break;
         };
     }
     while(quit!=1);
